e13969c9965592a52d81143661485dec9f717568
Meng Zhu <meng.zhu@nav.no>
2020-02-26 10:24:33 +0100

    validate enhet

--- packages/familie-backend/src/auth/bruker.ts
+++ packages/familie-backend/src/auth/bruker.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import request from 'request-promise';
 import { ITokenRequest } from '../typer';
 import { hentOnBehalfOfToken } from './token';
-import { logInfo } from '../customLoglevel';
+import { logInfo, logError } from '../customLoglevel';
 
 // Hent brukerprofil
 export const hentBrukerprofil = () => {
@@ -24,7 +24,7 @@ export const hentBrukerprofil = () => {
 
 // Hent brukerenhet
 export const hentBrukerenhet = (saksbehandlerTokenConfig: ITokenRequest) => {
-    
+
     const msGraphOBOTokenConfig: ITokenRequest = {
         clientId: saksbehandlerTokenConfig.clientId,
         clientSecret: saksbehandlerTokenConfig.clientSecret,
@@ -32,47 +32,58 @@ export const hentBrukerenhet = (saksbehandlerTokenConfig: ITokenRequest) => {
         scope: `https://graph.microsoft.com/.default`,
         tokenUri: saksbehandlerTokenConfig.tokenUri,
     };
-    
-    return async (req:Request, _: Response, next: NextFunction) => {
 
-        const msGraphMeUrl= `https://graph.microsoft.com/v1.0/me`
-        if(!req.session){
+    return async (req: Request, _: Response, next: NextFunction) => {
+        const ukjentEnhet = "9999";
+
+        const msGraphMeUrl = `https://graph.microsoft.com/v1.0/me`
+        if (!req.session) {
             throw new Error('Mangler sesjon på kall');
         }
 
-        if(req.session.enhet){
+        if (req.session.enhet) {
             logInfo(req, "Gyldig enhet i session");
             return next();
         }
 
-        logInfo(req, "Enhet ikke i session, henter fra "+ msGraphMeUrl);
-        
-        const obotoken= await hentOnBehalfOfToken(req, saksbehandlerTokenConfig, msGraphOBOTokenConfig);
+        logInfo(req, "Enhet ikke i session, henter fra " + msGraphMeUrl);
+
+        const obotoken = await hentOnBehalfOfToken(req, saksbehandlerTokenConfig, msGraphOBOTokenConfig);
 
         request
-        .get(
-            { url: msGraphMeUrl, headers:{
-                Authorization: 'Bearer '+ obotoken,
-            } },
-            (_err, _httpResponse, body) => {
-                return body;
-            },
-        )
-        .then(result => {
-            const enhet= JSON.parse(result)?.officeLocation?.slice(0, 4);
-            if(!enhet){
-                throw new Error('officeLocation ikke funnet fra Microsoft Graph, eller formatet er uventet');
-            }
-            
-            if(!req.session){
-                throw new Error('Mangler sesjon på kall');
-            }
+            .get(
+                {
+                    url: msGraphMeUrl, headers: {
+                        Authorization: 'Bearer ' + obotoken,
+                    }
+                },
+                (_err, _httpResponse, body) => {
+                    return body;
+                },
+            )
+            .then(result => {
+                var officeLocation = JSON.parse(result)?.officeLocation;
+                if (!officeLocation || !(/^\d{4}\s+\S+.*$/.test(officeLocation))) {
+                    logError(req, 'officeLocation ikke funnet fra Microsoft Graph, eller formatet er uventet: ' + officeLocation);
+                    officeLocation = ukjentEnhet;
+                }
 
-            req.session.enhet= enhet;
-            return next();
-        })
-        .catch(err => {
-            throw new Error(`Feilet ved hent enhet: ${err}`);
-        });
+                if (!req.session) {
+                    throw new Error('Mangler sesjon på kall');
+                }
+
+                req.session.enhet = officeLocation.slice(0, 4);
+                return next();
+            })
+            .catch(err => {
+                logError(req, `Feilet ved hent enhet: ${err}`);
+                if (!req.session) {
+                    throw new Error('Mangler sesjon på kall');
+                }
+
+                req.session.enhet = ukjentEnhet;
+
+                return next();
+            });
     };
 };
