084d8da4a4fface94902a1cf77e9de077e2f5183
Meng Zhu <meng.zhu@nav.no>
2020-03-19 22:50:07 +0100

    trigger build

--- packages/familie-header/src/icons/Ikon.tsx
+++ packages/familie-header/src/icons/Ikon.tsx
@@ -8,7 +8,7 @@ export interface IkonProps {
     children?: ReactNode | ReactNode[];
 }
 
-const Ikon = ({ children, width = 16, height = 16, viewBox = 24 }: IkonProps) => {
+const Ikon = ({ children , width = 16, height = 16, viewBox = 24 }: IkonProps) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox={`0 0 ${viewBox} ${viewBox}`}>
             {children}
