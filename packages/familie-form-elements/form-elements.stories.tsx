18ce52288c5559e560749f440edc4e181a29b997
hanngu <hanne@informatikk.org>
2020-06-11 15:54:30 +0200

    legg til readme og storybook

---
+++ packages/familie-form-elements/form-elements.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FamilieCheckbox } from './src';
+
+export default {
+    component: FamilieCheckbox,
+    parameters: {
+        componentSubtitle: 'Visittkort brukes til å vise enkel informasjon om en bruker.',
+    },
+    title: 'Komponenter/Visittkort',
+};
+
+export const familieCheckbox = () => {
+    return (
+        <FamilieCheckbox
+            label={"label"}
+            checked={false}
+            erLesevisning={true}
+        />
+    );
+};
