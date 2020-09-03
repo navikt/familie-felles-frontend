d9ab47e73a84de2478599a4601d129cae4c09a45
Henning Håkonsen <henninghaakonsen@me.com>
2020-09-03 13:11:31 +0200

    Forslag til løsning for å vise bedre tekst på lesevisning av familie-select.

--- packages/familie-form-elements/src/select/FamilieSelect.tsx
+++ packages/familie-form-elements/src/select/FamilieSelect.tsx
@@ -4,6 +4,7 @@ import { FamilieLesefelt } from '../lesefelt';
 
 export interface IFamilieSelectProps extends SelectProps {
     erLesevisning?: boolean;
+    lesevisningVerdi?: string;
 }
 
 export const FamilieSelect: React.FC<IFamilieSelectProps> = ({
@@ -11,12 +12,13 @@ export const FamilieSelect: React.FC<IFamilieSelectProps> = ({
     children,
     erLesevisning = false,
     label,
+    lesevisningVerdi,
     name,
     onChange,
     value,
 }) => {
     return erLesevisning ? (
-        <FamilieLesefelt label={label} verdi={value} />
+        <FamilieLesefelt label={label} verdi={lesevisningVerdi ? lesevisningVerdi : value} />
     ) : (
         <Select name={name} label={label} bredde={bredde} value={value} onChange={onChange}>
             {children}
