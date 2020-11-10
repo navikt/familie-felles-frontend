2fa0634b2a118de73887dabe4f218d147a569afb
Daphne Leebeek <daphne.leebeek@nav.no>
2020-11-10 16:51:34 +0100

    Denne legger til className som prop på infokort

--- packages/familie-header/src/søk/Infokort.tsx
+++ packages/familie-header/src/søk/Infokort.tsx
@@ -9,6 +9,7 @@ export interface IInfokortProps {
     index: number;
     onClick?: (index: number) => void;
     children?: React.ReactNode | React.ReactNode[];
+    className?: string;
 }
 
 export const Infokort: React.FunctionComponent<IInfokortProps> = ({
@@ -17,13 +18,16 @@ export const Infokort: React.FunctionComponent<IInfokortProps> = ({
     index,
     onClick,
     children,
+    className,
 }) => {
     const onclick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         onClick?.(+event.currentTarget.id);
     };
 
     return (
-        <div className={'infokort' + (index % 2 ? ' infokort__odd' : ' infokort__even')}>
+        <div
+            className={'infokort' + (index % 2 ? ' infokort__odd' : ' infokort__even') + className}
+        >
             <div className="infokort__ikon">{ikon}</div>
             <div className="infokort__content" onClick={onclick} id={index.toString()}>
                 <div className="infokort_header">{header}</div>
