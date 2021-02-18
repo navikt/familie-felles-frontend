c5c2213c2956ef95048d8bddba751687965c144c
alexandraback <alexandra1.back@gmail.com>
2021-02-18 09:45:28 +0100

    Legger til string til typen av fagsakId slik att komponenten kan tas i bruk i ef

--- packages/familie-header/src/types/søkeresultat.ts
+++ packages/familie-header/src/types/søkeresultat.ts
@@ -2,7 +2,7 @@ import { Adressebeskyttelsegradering } from '@navikt/familie-typer';
 
 export interface ISøkeresultat {
     adressebeskyttelseGradering?: Adressebeskyttelsegradering;
-    fagsakId?: number; // null betyr at det ikke finnes fagsak på personen
+    fagsakId?: number | string; // null betyr at det ikke finnes fagsak på personen
     harTilgang: boolean;
     ident: string;
     ikon: React.ReactNode;
