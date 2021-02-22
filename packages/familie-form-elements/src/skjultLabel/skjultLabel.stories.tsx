e525b145a278246b81ca5f773c01abd757a1fb50
Daphne <daphne.leebeek@nav.no>
2021-02-22 13:40:11 +0100

    feat(Skjult label): en skjult label som leses av skjermleser men som … (#273)
    * feat(Skjult label): en skjult label som leses av skjermleser men som ikke vises
    
    affects: @navikt/familie-form-elements

---
+++ packages/familie-form-elements/src/skjultLabel/skjultLabel.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import SkjultLabel from './SkjultLabel';
+import { Input } from 'nav-frontend-skjema';
+import { Normaltekst } from 'nav-frontend-typografi';
+
+export default {
+    component: SkjultLabel,
+    parameters: {
+        componentSubtitle: 'En skjult label som kun vises i htmldom-strukturen for skjermlesere.',
+    },
+    title: 'Komponenter/Form-elementer/SkjultLabel',
+};
+
+export const SkjultLabelStory: React.FC = () => {
+    return (
+        <>
+            <Normaltekst>
+                I noen tilfeller passer det ikke alltid i layouten med en tilhørende label som er
+                synlig til hvert form-element. Likevel er det viktig at koden har en label, slik at
+                de som bruker skjermleser får opp form-elements i riktig kontekst. Denne komponenten
+                legger på en skjult label, som du vil se hvis du åpner inspect.
+            </Normaltekst>
+            <SkjultLabel htmlFor="input" />
+            <Input id="input" />
+        </>
+    );
+};
