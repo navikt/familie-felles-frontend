b4b25f66bb328ee9c131bba51b8a90b3fc48894e
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-04-13 11:49:06 +0200

    fix(familie-form-elements): endret til å bruke knobs-addon fra workspace
    affects: @navikt/familie-form-elements
    
    Knobs funker ikke hvis det ikke er versjonen fra prosjektet, så fjernet som dev dependency fra
    pakken

--- packages/familie-form-elements/src/ja-nei-spørsmål/jaNeiSpørsmål.stories.tsx
+++ packages/familie-form-elements/src/ja-nei-spørsmål/jaNeiSpørsmål.stories.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { JaNeiSpørsmål } from './JaNeiSpørsmål';
 import { ESvar } from './typer';
 import styled from "styled-components";
+// Det må faktisk være plugin fra workspace her så vidt jeg forstår
+// tslint:disable-next-line:no-implicit-dependencies
 import { number, withKnobs } from "@storybook/addon-knobs";
-import { NumberTypeKnobOptions } from "@storybook/addon-knobs/dist/components/types";
 
 export default {
     component: JaNeiSpørsmål,
@@ -18,38 +19,32 @@ const DivMedBredde = styled.div<{bredde: string}>`
   width: ${props => props.bredde};
 `
 
-const JaNeiMedBreddeToggle: React.FC = ({children}) => {
-  const knobOptions: NumberTypeKnobOptions = {
-    min: 10,
-    max: 100,
-    step: 1,
-    range: true
-  }
-  return (
-    <>
-      <DivMedBredde bredde={number('Breddebegrensning', 100, knobOptions).toString() + '%'}>
-        {children}
-      </DivMedBredde>
-    </>
-  )
-}
-
 export const FamilieJaNeiSpørsmålStory: React.FC = () => {
+    const bredde = number(
+        'Breddebegrensning',
+        100,
+        {
+            min: 10,
+            max: 100,
+            step: 1,
+            range: true
+        }
+    )
     return (
-      <>
-        <JaNeiMedBreddeToggle>
-          <JaNeiSpørsmål
-            labelTekstForJaNei={{ ja: 'Ja', nei: 'Nei' }}
-            legend={'Har du det bra?'}
-            name={'person.hardudetbra'}
-            onChange={value => {
-              alert(value === ESvar.NEI ? 'So sad' : 'Yayy, hurra!');
-            }}
-          />
-        </JaNeiMedBreddeToggle>
-        <div>
-          Du kan begrense bredde via knobs
-        </div>
-      </>
+        <>
+            <DivMedBredde bredde={bredde.toString(10) + '%'}>
+                <JaNeiSpørsmål
+                    labelTekstForJaNei={{ ja: 'Ja', nei: 'Nei' }}
+                    legend={'Har du det bra?'}
+                    name={'person.hardudetbra'}
+                    onChange={value => {
+                        alert(value === ESvar.NEI ? 'So sad' : 'Yayy, hurra!');
+                    }}
+                />
+            </DivMedBredde>
+            <div>
+                Du kan begrense bredde via knobs
+            </div>
+        </>
     );
 };
