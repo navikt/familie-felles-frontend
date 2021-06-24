4f102f4642c7f6991445b28fae31ca119bc4f176
Daphne Leebeek <daphne.leebeek@nav.no>
2021-06-24 12:04:14 +0200

    feat(Ref JaNeiSpm): gjør mulig å lage en ref til jaNeiSpm

--- packages/familie-form-elements/src/ja-nei-spørsmål/JaNeiSpørsmål.tsx
+++ packages/familie-form-elements/src/ja-nei-spørsmål/JaNeiSpørsmål.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useState, Ref } from 'react';
 import { RadioPanelGruppe } from 'nav-frontend-skjema';
 import styled from 'styled-components';
 import { ESvar } from './typer';
@@ -14,57 +14,61 @@ export interface JaNeiSpørsmålProps {
     legend: ReactNode;
     name: string;
     labelTekstForRadios: LabelTekstForJaNei;
-    initiellVerdi?: ESvar | undefined;
+    initiellVerdi: ESvar | null;
     feil?: ReactNode;
 }
 
 const StyledRadioPanelGruppe = styled(RadioPanelGruppe)`
-  && label:not(:last-child) {
-    margin-bottom: 1rem;
-  }
+    && label:not(:last-child) {
+        margin-bottom: 1rem;
+    }
 `;
 
 const Capitalized = styled.span`
-  ::first-letter{
-    text-transform: capitalize;
-  }
+    ::first-letter {
+        text-transform: capitalize;
+    }
 `;
 
-export const JaNeiSpørsmål: React.FC<JaNeiSpørsmålProps> = ({
-    legend,
-    name,
-    onChange,
-    labelTekstForRadios,
-    feil,
-    initiellVerdi,
-}) => {
-    const [checked, setChecked] = useState<ESvar | undefined>(initiellVerdi);
+export const JaNeiSpørsmål = React.forwardRef(
+    (props: JaNeiSpørsmålProps, ref: Ref<RadioPanelGruppe> | undefined) => {
+        const { legend, name, onChange, labelTekstForRadios, feil, initiellVerdi } = props;
 
-    let radios = [
-        { label: <Capitalized>{labelTekstForRadios.ja}</Capitalized>, value: ESvar.JA },
-        {
-            label: <Capitalized>{labelTekstForRadios.nei}</Capitalized>,
-            value: ESvar.NEI,
-        },
-    ]
+        const [checked, setChecked] = useState<ESvar | null>(initiellVerdi);
 
-    const inkluderVetIkke = !!labelTekstForRadios.vetikke;
+        let radios = [
+            { label: <Capitalized>{labelTekstForRadios.ja}</Capitalized>, value: ESvar.JA },
+            {
+                label: <Capitalized>{labelTekstForRadios.nei}</Capitalized>,
+                value: ESvar.NEI,
+            },
+        ];
 
-    if (inkluderVetIkke) {
-        radios = [...radios, { label: <Capitalized>{labelTekstForRadios.vetikke}</Capitalized>, value: ESvar.VET_IKKE }];
-    }
+        const inkluderVetIkke = !!labelTekstForRadios.vetikke;
 
-    return (
-        <StyledRadioPanelGruppe
-            legend={legend}
-            name={name}
-            radios={radios}
-            checked={checked}
-            onChange={(_event, value) => {
-                setChecked(value);
-                onChange(value);
-            }}
-            feil={feil}
-        />
-    );
-};
+        if (inkluderVetIkke) {
+            radios = [
+                ...radios,
+                {
+                    label: <Capitalized>{labelTekstForRadios.vetikke}</Capitalized>,
+                    value: ESvar.VET_IKKE,
+                },
+            ];
+        }
+
+        return (
+            <StyledRadioPanelGruppe
+                legend={legend}
+                name={name}
+                radios={radios}
+                checked={checked ?? undefined}
+                onChange={(_event, value) => {
+                    setChecked(value);
+                    onChange(value);
+                }}
+                feil={feil}
+                ref={ref}
+            />
+        );
+    },
+);
