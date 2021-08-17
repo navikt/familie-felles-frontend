f20b2fe431d4f544a25638bc274e07042fa2273e
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-08-17 12:09:33 +0200

    Legg til muligheten til å legge til lenke på Visittkort

--- packages/familie-visittkort/src/index.tsx
+++ packages/familie-visittkort/src/index.tsx
@@ -2,6 +2,7 @@ import Clipboard from '@navikt/familie-clipboard';
 import { FamilieIkonVelger } from '@navikt/familie-ikoner';
 import { kjønnType } from '@navikt/familie-typer';
 import Element, { Normaltekst } from 'nav-frontend-typografi';
+import Lenke from 'nav-frontend-lenker';
 import * as React from 'react';
 
 import './index.less';
@@ -11,13 +12,27 @@ export interface IProps {
     ident: string;
     kjønn: kjønnType;
     navn: string;
+    href?: string;
 }
 
-const Visittkort: React.StatelessComponent<IProps> = ({ alder, children, ident, kjønn, navn }) => {
+const Visittkort: React.StatelessComponent<IProps> = ({
+    alder,
+    children,
+    ident,
+    kjønn,
+    navn,
+    href,
+}) => {
     return (
         <div className={'visittkort'}>
             <FamilieIkonVelger className={'visittkort__ikon'} alder={alder} kjønn={kjønn} />
-            <Element type={'element'}>{navn}</Element>
+            {href ? (
+                <Lenke href={href}>
+                    <Element type={'element'}>{navn}</Element>
+                </Lenke>
+            ) : (
+                <Element type={'element'}>{navn}</Element>
+            )}
 
             <div className={'visittkort__pipe'}>|</div>
 
