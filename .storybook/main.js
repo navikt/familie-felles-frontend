0f1f001fec62dc30cd37987bf8de69f6513f4d77
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-01-26 12:18:56 +0100

    Fjern ubrukt kode

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,16 +7,9 @@ module.exports = {
         reactDocgen: 'react-docgen-typescript',
         reactDocgenTypescriptOptions: {
             shouldExtractLiteralValuesFromEnum: true,
-            // propFilter: (prop) => ['label', 'disabled'].includes(prop.name),
         },
     },
     webpackFinal: async config => {
-        // config.module.rules.push({
-        //     test: /\.(ts|tsx)$/,
-        //     use: [
-        //         { loader: require.resolve('react-docgen-typescript-loader') },
-        //     ],
-        // });
         config.module.rules.push({
             test: /\.(less)$/,
             use: [
