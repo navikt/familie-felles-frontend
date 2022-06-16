3ab064bdb984692af010f479c5a5c99354154a6c
tnarland <47184872+tnarland@users.noreply.github.com>
2022-06-16 15:53:16 +0200

    Utvider Visittkort med valgfritt ikon-argument (#466)
    Vil brukes i famile-ba-sak til å sette eget ikon for saker som gjelder institusjon

--- packages/familie-visittkort/src/index.tsx
+++ packages/familie-visittkort/src/index.tsx
@@ -6,17 +6,34 @@ import * as React from 'react';
 
 import './index.less';
 
+export interface IkonProps {
+    className?: string;
+    heigth?: number;
+    width?: number;
+}
 export interface IProps {
     alder: number;
     ident: string;
     kjønn: kjønnType;
     navn: string | React.ReactNode;
+    ValgfrittIkon?: React.ComponentType<IkonProps>;
 }
 
-export const Visittkort: React.StatelessComponent<IProps> = ({ alder, children, ident, kjønn, navn }) => {
+export const Visittkort: React.FunctionComponent<IProps> = ({
+    alder,
+    children,
+    ident,
+    kjønn,
+    navn,
+    ValgfrittIkon,
+}) => {
     return (
         <div className={'visittkort'}>
-            <FamilieIkonVelger className={'visittkort__ikon'} alder={alder} kjønn={kjønn} />
+            {ValgfrittIkon ? (
+                <ValgfrittIkon className={'visittkort__ikon'} width={32} heigth={32} />
+            ) : (
+                <FamilieIkonVelger className={'visittkort__ikon'} alder={alder} kjønn={kjønn} />
+            )}
             {typeof navn === 'string' ? <Element type={'element'}>{navn}</Element> : navn}
 
             <div className={'visittkort__pipe'}>|</div>
