b6b1ad924f130df71fd3d568ae91622f8f74eafc
Viktor <viktor.grondalen.solberg@nav.no>
2023-08-18 14:08:17 +0200

    feat(faser ut @navikt/ds-icons og tar i bruk @navikt/aksel-icons for familie-clipboard. Tar i bruk @
    affects: @navikt/familie-clipboard, @navikt/familie-visittkort
    
    BREAKING CHANGE:
    Krever versjon 5 av @navikt/aksel-icons for familie-clipboard og familie-visittkort

--- packages/familie-clipboard/src/ClipboardIcon.tsx
+++ packages/familie-clipboard/src/ClipboardIcon.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Copy, SuccessColored } from '@navikt/ds-icons';
+import { CheckmarkCircleIcon, FilesIcon } from '@navikt/aksel-icons';
 
 interface IProps {
     type: 'check' | 'copy';
     size?: number;
 }
 
-const ClipboardIcon = ({ type, size = 20 }: IProps) => {
+const ClipboardIcon = ({ type, size = 24 }: IProps) => {
     return type === 'check' ? (
-        <SuccessColored
+        <CheckmarkCircleIcon
             fr="mask"
             height={size}
             width={size}
@@ -16,7 +16,7 @@ const ClipboardIcon = ({ type, size = 20 }: IProps) => {
             onResizeCapture={undefined}
         />
     ) : (
-        <Copy
+        <FilesIcon
             fr="mask"
             height={size}
             width={size}
