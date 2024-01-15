1f65d915496fa28deefaf77ecb3e6ec7523a3188
Kristine Steine <kristine.steine@bekk.no>
2024-01-15 14:51:16 +0100

    feat(familievisittkort): skriv om til å bruke Aksel-primitiver for komponentlayout
    BREAKING CHANGE: Layout av children-elementer styres nå av konsumentene

--- packages/familie-visittkort/src/index.tsx
+++ packages/familie-visittkort/src/index.tsx
@@ -1,42 +1,30 @@
-import { FamilieIkonVelger } from '@navikt/familie-ikoner';
-import { kjønnType } from '@navikt/familie-typer';
 import * as React from 'react';
 import styled from 'styled-components';
-import { CopyButton, Label } from '@navikt/ds-react';
-import { AGray700, ASpacing2, ASpacing4 } from '@navikt/ds-tokens/dist/tokens';
+import { CopyButton, HStack, Label } from '@navikt/ds-react';
+import { ABorderStrong } from '@navikt/ds-tokens/dist/tokens';
+import { FamilieIkonVelger } from '@navikt/familie-ikoner';
+import { kjønnType } from '@navikt/familie-typer';
 
 export interface IkonProps {
     className?: string;
-    heigth?: number;
+    height?: number;
     width?: number;
 }
-export interface IProps {
+export interface IProps extends React.PropsWithChildren {
     alder: number;
     ident: string;
     kjønn: kjønnType;
     navn: string | React.ReactNode;
     ValgfrittIkon?: React.ComponentType<IkonProps>;
-    children?: React.ReactNode;
 }
 
-const StyledVisittkort = styled.div`
-    border-bottom: 1px solid ${AGray700};
+const StyledVisittkort = styled(HStack)`
+    border-bottom: 1px solid ${ABorderStrong};
     height: 3rem;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    .visittkort__ikon {
-        padding-right: ${ASpacing2};
-    }
-    .visittkort__pipe {
-        padding: 0 ${ASpacing4};
-    }
 `;
 
-const FlexBox = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 0.25rem;
+const GrådigChildrenContainer = styled.div`
+    flex: 1;
 `;
 
 export const Visittkort: React.FunctionComponent<IProps> = ({
@@ -48,28 +36,27 @@ export const Visittkort: React.FunctionComponent<IProps> = ({
     ValgfrittIkon,
 }) => {
     return (
-        <StyledVisittkort className={'visittkort'}>
-            {ValgfrittIkon ? (
-                <ValgfrittIkon className={'visittkort__ikon'} width={32} heigth={32} />
-            ) : (
-                <FamilieIkonVelger className={'visittkort__ikon'} alder={alder} kjønn={kjønn} />
-            )}
-            {typeof navn === 'string' ? (
-                <Label size={'small'}>
-                    {navn} ({alder} år)
-                </Label>
-            ) : (
-                navn
-            )}
-
-            <div className={'visittkort__pipe'}>|</div>
-
-            <FlexBox>
-                {ident}
-                <CopyButton copyText={ident.replace(' ', '')} size={'small'} />
-            </FlexBox>
-
-            {children}
+        <StyledVisittkort align="center" justify="space-between" gap="4">
+            <HStack align="center" gap="4">
+                {ValgfrittIkon ? (
+                    <ValgfrittIkon width={24} height={24} />
+                ) : (
+                    <FamilieIkonVelger alder={alder} kjønn={kjønn} width={24} height={24} />
+                )}
+                {typeof navn === 'string' ? (
+                    <Label size={'small'}>
+                        {navn} ({alder} år)
+                    </Label>
+                ) : (
+                    navn
+                )}
+                <div>|</div>
+                <HStack align="center" gap="1">
+                    {ident}
+                    <CopyButton copyText={ident.replace(' ', '')} size={'small'} />
+                </HStack>
+            </HStack>
+            <GrådigChildrenContainer>{children}</GrådigChildrenContainer>
         </StyledVisittkort>
     );
 };
