1e55343da5df7c4fd575761ee5e16391ce13c669
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-07-02 14:03:00 +0200

    feat(skjultlabel): sletter SkjultLabel. Bruk designsystemet istedet

--- packages/familie-form-elements/src/index.ts
+++ packages/familie-form-elements/src/index.ts
@@ -3,5 +3,4 @@ export * from './måned-år-velger';
 export * from './radiogruppe';
 export * from './select';
 export * from './familie-react-select';
-export * from './skjultLabel';
 export * from './ja-nei-spørsmål';
