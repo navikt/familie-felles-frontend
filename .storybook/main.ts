af3267ba0d06bda80af38fa922fa09b3f6c03e86
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2024-08-08 10:24:04 +0200

    Oppgradert avhengigheter og fjernet noen unødvendige avhengigheter (#1554)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,38 +1,41 @@
-import { dirname, join } from "path";
+import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
-
 const storybookConfig: StorybookConfig = {
-        stories: ['../packages/familie-*/src/**/*.@(mdx|stories.@(tsx))', '../packages/familie-*/*.@(mdx|stories.@(tsx))'],
-        addons: [
-            getAbsolutePath("@storybook/addon-essentials"),
-            getAbsolutePath("@storybook/addon-a11y"),
-            getAbsolutePath("@storybook/addon-storysource"),
-            getAbsolutePath("@storybook/addon-mdx-gfm"),
-            '@storybook/addon-webpack5-compiler-babel'
-        ],
-        typescript: {
-            check: true,
-            checkOptions: {},
-            reactDocgen: 'react-docgen-typescript',
-            reactDocgenTypescriptOptions: {
-                shouldExtractLiteralValuesFromEnum: true,
-            },
+    stories: [
+        '../packages/familie-*/src/**/*.@(mdx|stories.@(tsx))',
+        '../packages/familie-*/*.@(mdx|stories.@(tsx))',
+    ],
+    addons: [
+        getAbsolutePath('@storybook/addon-essentials'),
+        getAbsolutePath('@storybook/addon-a11y'),
+        getAbsolutePath('@storybook/addon-storysource'),
+        '@storybook/addon-webpack5-compiler-babel',
+    ],
+    typescript: {
+        check: true,
+        checkOptions: {},
+        reactDocgen: 'react-docgen-typescript',
+        reactDocgenTypescriptOptions: {
+            shouldExtractLiteralValuesFromEnum: true,
         },
-        framework: {
-            name: getAbsolutePath("@storybook/react-webpack5"),
-            options: {},
-        },
-        docs: {
-            autodocs: true,
-        },
-        async webpackFinal(config) {
-            if (config.module && config.module.rules) {
-                config.module.rules.push({
-                    test: /\.(less)$/,
-                    use: [{
+    },
+    framework: {
+        name: getAbsolutePath('@storybook/react-webpack5'),
+        options: {},
+    },
+    docs: {
+        autodocs: true,
+    },
+    async webpackFinal(config) {
+        if (config.module && config.module.rules) {
+            config.module.rules.push({
+                test: /\.(less)$/,
+                use: [
+                    {
                         loader: 'style-loader',
-                    }, {
+                    },
+                    {
                         loader: 'css-loader',
                         options: {
                             modules: {
@@ -40,28 +43,26 @@ const storybookConfig: StorybookConfig = {
                             },
                             importLoaders: 1,
                         },
-                    }, {
+                    },
+                    {
                         loader: require.resolve('less-loader'),
-                    }],
-                });
-                config.module.rules.push({
-                    test: /\.mjs$/,
-                    include: /node_modules/,
-                    type: 'javascript/auto',
-                });
-            }
-            if (config.resolve && config.resolve.extensions) {
-                config.resolve.extensions.push('.less');
-
-            }
-            return config;
-        },
-
-    }
-;
-
+                    },
+                ],
+            });
+            config.module.rules.push({
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto',
+            });
+        }
+        if (config.resolve && config.resolve.extensions) {
+            config.resolve.extensions.push('.less');
+        }
+        return config;
+    },
+};
 export default storybookConfig;
 
 function getAbsolutePath(value: string): any {
-    return dirname(require.resolve(join(value, "package.json")));
+    return dirname(require.resolve(join(value, 'package.json')));
 }
