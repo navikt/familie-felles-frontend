6d0c02b80f8495700ea198a2fcd1185b5b91138c
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2024-11-01 13:40:55 +0100

    feat(familie-backend/headers.ts): legg til Nav telemetry URL-er i CSP headers
    Nødvendig for å samle inn telemetri i appene våre

--- packages/familie-backend/src/headers.ts
+++ packages/familie-backend/src/headers.ts
@@ -5,8 +5,10 @@ const fontSource = 'https://fonts.gstatic.com';
 const navFontSource = 'https://cdn.nav.no';
 const amplitude = 'https://amplitude.nav.no';
 const sentry = 'https://sentry.gc.nav.no';
+const navTelemetry = 'https://telemetry.nav.no';
+const navTelemetryDev = 'https://telemetry.ekstern.dev.nav.no';
 
-const cspString = `default-src 'self' data: ${amplitude} ${sentry}; style-src 'self' ${styleSource} data: 'unsafe-inline'; font-src 'self' ${fontSource} ${navFontSource} data:; frame-src 'self' blob:;`;
+const cspString = `default-src 'self' data: ${amplitude} ${sentry} ${navTelemetry} ${navTelemetryDev}; style-src 'self' ${styleSource} data: 'unsafe-inline'; font-src 'self' ${fontSource} ${navFontSource} data:; frame-src 'self' blob:;`;
 
 const setup = (app: Express) => {
     app.disable('x-powered-by');
