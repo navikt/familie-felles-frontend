8609134861b08693b7678a2663d13e309bb63967
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2025-01-07 14:35:17 +0100

    fix(familie-endringslogg): feilende transition-bibliotek (#1597)
    Må fjerne transition-bibliotek pga feilsituasjoner i react 18 - og det hele kan løses med css
    
    BREAKING CHANGE: Bumpet til react 19

--- packages/familie-endringslogg/src/transition-container.tsx
+++ packages/familie-endringslogg/src/transition-container.tsx
@@ -1,15 +1,13 @@
-import React, { PropsWithChildren } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React from 'react';
 import { useFocus } from './hooks/use-focus';
 import './endringslogg.css';
 import './collapse-container-transition.css';
 import classNames from 'classnames';
-import { TransitionGroupProps } from 'react-transition-group/TransitionGroup';
-import { CSSTransitionProps } from 'react-transition-group/CSSTransition';
 
 interface CollapseContainerProps {
     children?: React.ReactNode;
     alignLeft?: boolean;
+    visible: boolean;
 }
 
 interface TransitionProps extends CollapseContainerProps {
@@ -17,44 +15,23 @@ interface TransitionProps extends CollapseContainerProps {
     alignLeft?: boolean;
 }
 
-/**
- * Overrider props ettersom transition-group ikke er oppgradert til React 18.
- * Disse to linjene kan fjernes når dette skjer
- */
-const TransitionGroupWithChildren = TransitionGroup as unknown as React.FC<
-    PropsWithChildren<TransitionGroupProps>
->;
-const CSSTransitionWithChildren = CSSTransition as unknown as React.FC<
-    PropsWithChildren<CSSTransitionProps>
->;
-
 const TransitionContainer = (props: TransitionProps) => (
-    <TransitionGroupWithChildren component={null}>
-        {props.visible && (
-            <CSSTransitionWithChildren
-                classNames={{
-                    enter: 'collapse-container-enter',
-                    enterActive: 'collapse-container-enter-active',
-                    exit: 'collapse-container-exit',
-                    exitActive: 'collapse-container-exit-active',
-                }}
-                timeout={400}
-            >
-                <CollapseContainer alignLeft={props.alignLeft}>{props.children}</CollapseContainer>
-            </CSSTransitionWithChildren>
-        )}
-    </TransitionGroupWithChildren>
+    <CollapseContainer alignLeft={props.alignLeft} visible={props.visible}>
+        {props.children}
+    </CollapseContainer>
 );
 
 const CollapseContainer = (props: CollapseContainerProps) => {
     const { focusRef } = useFocus();
     return (
         <div
-            className={
-                props.alignLeft
-                    ? classNames('align-left', 'collapse-container')
-                    : 'collapse-container'
-            }
+            className={classNames(
+                props.alignLeft && 'align-left',
+                props.visible
+                    ? 'collapse-container-enter-active'
+                    : 'collapse-container-exit-active',
+                'collapse-container',
+            )}
         >
             <div
                 className={props.alignLeft ? 'arrow-container-left' : 'arrow-container'}
