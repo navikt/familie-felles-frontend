9db933f7b0cec09c279c7e004c6fd611156adab1
Hanne Olsen <hanne.olsen@nav.no>
2025-03-31 12:02:19 +0200

    fix(familie-http): Fjerner constate, useHttp må nå alltid brukes inni en HttpProvider (#1619)
    * BREAKING CHANGE: Fjerner constate, useHttp må nå alltid brukes inni en HttpProvider
    
    * Fjerner constate fra yarn.lock

--- packages/familie-http/src/HttpProvider.tsx
+++ packages/familie-http/src/HttpProvider.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { createContext, PropsWithChildren, useContext } from 'react';
 import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
-import constate from 'constate';
 import { Ressurs, ApiRessurs, ISaksbehandler } from '@navikt/familie-typer';
 import { preferredAxios, håndterApiRespons } from './axios';
 
@@ -15,70 +14,91 @@ export type FamilieRequest = <SkjemaData, SkjemaRespons>(
     config: FamilieRequestConfig<SkjemaData>,
 ) => Promise<Ressurs<SkjemaRespons>>;
 
+interface HttpContext {
+    systemetLaster: () => boolean;
+    request: FamilieRequest;
+}
+
+const HttpContext = createContext<HttpContext | undefined>(undefined);
+
 interface IProps {
     fjernRessursSomLasterTimeout?: number;
     innloggetSaksbehandler?: ISaksbehandler;
     settAutentisert?: (autentisert: boolean) => void;
 }
 
-export const [HttpProvider, useHttp] = constate(
-    ({ innloggetSaksbehandler, settAutentisert, fjernRessursSomLasterTimeout = 300 }: IProps) => {
-        const [ressurserSomLaster, settRessurserSomLaster] = React.useState<string[]>([]);
+export function HttpProvider(props: PropsWithChildren<IProps>) {
+    const [ressurserSomLaster, settRessurserSomLaster] = React.useState<string[]>([]);
+    const { innloggetSaksbehandler, settAutentisert } = props;
+    const fjernRessursSomLasterTimeout = 300;
 
-        const fjernRessursSomLaster = (ressursId: string) => {
-            setTimeout(() => {
-                settRessurserSomLaster((prevState: string[]) => {
-                    return prevState.filter((ressurs: string) => ressurs !== ressursId);
+    const fjernRessursSomLaster = (ressursId: string) => {
+        setTimeout(() => {
+            settRessurserSomLaster((prevState: string[]) => {
+                return prevState.filter((ressurs: string) => ressurs !== ressursId);
+            });
+        }, fjernRessursSomLasterTimeout);
+    };
+
+    const systemetLaster = () => {
+        return ressurserSomLaster.length > 0;
+    };
+
+    const request: FamilieRequest = async <SkjemaData, SkjemaRespons>(
+        config: FamilieRequestConfig<SkjemaData>,
+    ): Promise<Ressurs<SkjemaRespons>> => {
+        const ressursId = `${config.method}_${config.url}`;
+        config.påvirkerSystemLaster && settRessurserSomLaster([...ressurserSomLaster, ressursId]);
+
+        return preferredAxios
+            .request(config)
+            .then((response: AxiosResponse<ApiRessurs<SkjemaRespons>>) => {
+                const responsRessurs: ApiRessurs<SkjemaRespons> = response.data;
+
+                config.påvirkerSystemLaster && fjernRessursSomLaster(ressursId);
+                return håndterApiRespons({
+                    defaultFeilmelding: config.defaultFeilmelding,
+                    innloggetSaksbehandler,
+                    loggFeilTilSentry: config.loggFeilTilSentry,
+                    ressurs: responsRessurs,
                 });
-            }, fjernRessursSomLasterTimeout);
-        };
+            })
+            .catch((error: AxiosError<ApiRessurs<SkjemaRespons>>) => {
+                if (error.message.includes('401') && settAutentisert) {
+                    settAutentisert(false);
+                }
 
-        const systemetLaster = () => {
-            return ressurserSomLaster.length > 0;
-        };
+                config.påvirkerSystemLaster && fjernRessursSomLaster(ressursId);
 
-        const request: FamilieRequest = async <SkjemaData, SkjemaRespons>(
-            config: FamilieRequestConfig<SkjemaData>,
-        ): Promise<Ressurs<SkjemaRespons>> => {
-            const ressursId = `${config.method}_${config.url}`;
-            config.påvirkerSystemLaster &&
-                settRessurserSomLaster([...ressurserSomLaster, ressursId]);
-
-            return preferredAxios
-                .request(config)
-                .then((response: AxiosResponse<ApiRessurs<SkjemaRespons>>) => {
-                    const responsRessurs: ApiRessurs<SkjemaRespons> = response.data;
-
-                    config.påvirkerSystemLaster && fjernRessursSomLaster(ressursId);
-                    return håndterApiRespons({
-                        defaultFeilmelding: config.defaultFeilmelding,
-                        innloggetSaksbehandler,
-                        loggFeilTilSentry: config.loggFeilTilSentry,
-                        ressurs: responsRessurs,
-                    });
-                })
-                .catch((error: AxiosError<ApiRessurs<SkjemaRespons>>) => {
-                    if (error.message.includes('401') && settAutentisert) {
-                        settAutentisert(false);
-                    }
-
-                    config.påvirkerSystemLaster && fjernRessursSomLaster(ressursId);
-
-                    const responsRessurs: ApiRessurs<SkjemaRespons> | undefined =
-                        error.response?.data;
-                    return håndterApiRespons({
-                        defaultFeilmelding: config.defaultFeilmelding,
-                        error,
-                        innloggetSaksbehandler,
-                        loggFeilTilSentry: true,
-                        ressurs: responsRessurs,
-                    });
+                const responsRessurs: ApiRessurs<SkjemaRespons> | undefined = error.response?.data;
+                return håndterApiRespons({
+                    defaultFeilmelding: config.defaultFeilmelding,
+                    error,
+                    innloggetSaksbehandler,
+                    loggFeilTilSentry: true,
+                    ressurs: responsRessurs,
                 });
-        };
+            });
+    };
 
-        return {
-            systemetLaster,
-            request,
-        };
-    },
-);
+    return (
+        <HttpContext.Provider
+            value={{
+                systemetLaster,
+                request,
+            }}
+        >
+            {props.children}
+        </HttpContext.Provider>
+    );
+}
+
+export function useHttp() {
+    const context = useContext(HttpContext);
+
+    if (context === undefined) {
+        throw new Error('useHttp må brukes innenfor HttpProvider');
+    }
+
+    return context;
+}
